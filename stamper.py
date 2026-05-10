5b8ca8c559e17a9adfe7465f4b977df2faefcff4
Thomas Truong <TruongThomasQ@gmail.com>
2026-05-09 20:18:05 -0700

    refactor: remove messagebox warning for threshold

--- stamper.py
+++ stamper.py
@@ -13,7 +13,6 @@ import sys
 import threading
 import customtkinter as ctk
 from CTkToolTip import CTkToolTip
-from CTkMessagebox import CTkMessagebox
 from tkinter import filedialog
 from pathlib import Path
 from PIL import Image, ImageTk
@@ -757,16 +756,6 @@ class StamperApp(ctk.CTk):
 
     clicked_button.configure(fg_color=default_blue)
     self.search_dir = clicked_button.index
-
-    # Selected all directions and has very low threshold.
-    if clicked_button.index == 4 and self.threshold < 25:
-      self.after(200, lambda: CTkMessagebox(title="Warning",
-                    message="Threshold is low (< ~25) for searching all" +
-                            "directions. It will probably take a long" + 
-                            "time to search.",
-                    icon="warning",
-                    option_1 = "OK",
-                    master=self))
 
 
   def is_out_of_bounds(self, pos0, pos1, max_size, margin):
